1913344c6484296dc46299b9cc73fe66868e4c31
gurpreet <gurpreet.guller@bacancy.com>
2022-11-07 12:33:07 +0530

    added middleware and message pacl

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,25 @@
 package middleware
+
+import (
+	"crypto/rsa"
+	"fmt"
+
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// check/parse token and check messagepack header
+func CustomKeyFunc(priateKey *rsa.PrivateKey) jwt.Keyfunc {
+	return func(t *jwt.Token) (interface{}, error) {
+		// Always check the signing method
+		if t.Method.Alg() != jwtware.RS256 {
+			return nil, fmt.Errorf("unexpected jwt signing method=%v", t.Header["alg"])
+		}
+
+		// TODO checking request body type here
+
+		signingKey := priateKey.Public()
+
+		return signingKey, nil
+	}
+}
--- types/login.go
+++ types/login.go
@@ -1,1 +1,9 @@
 package types
+
+type UserLoginRequest struct {
+	Email    string `msg:"email"`
+	Password string `msg:"password"`
+}
+type UserLoginResponse struct {
+	Token string `msg:"token"`
+}
