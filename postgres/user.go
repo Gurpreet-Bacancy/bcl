238629a76fbf23ea25adddf50b5e3d2a4c026244
Aniket <aniket@Anikets-MacBook-Pro.local>
2022-11-07 14:41:43 +0530

    Added error handling for user

--- postgres/user.go
+++ postgres/user.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"github.com/Gurpreet-Bacancy/bcl/model"
+	"github.com/jinzhu/gorm"
 )
 
 type UserManager struct {
@@ -10,8 +11,14 @@ type UserManager struct {
 
 func (am UserManager) GetUserByEmail(email string) (model.UserItem, error) {
 	var userItems model.UserItem
-
+	tx := am.app.db.DB.Where("email=?", email).First(&userItems)
+	if tx.RecordNotFound() {
+		return model.UserItem{}, gorm.ErrRecordNotFound
+	} else if tx.Error != nil {
+		return model.UserItem{}, tx.Error
+	}
 	return userItems, nil
+
 }
 
 func (am UserManager) GetUserByID(userID uint) (model.UserItem, error) {
