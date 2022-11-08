b89da7bb3ccf41bc1ce987d9b00224c74ffb41e3
moksh-solanki <moksh.solanki@bacancy.com>
2022-11-08 19:55:23 +0530

    middleware modified

--- helper/helper.go
+++ helper/helper.go
@@ -1,1 +1,11 @@
 package helper
+
+import "github.com/gofiber/fiber/v2"
+
+func HandleError(c *fiber.Ctx, code int, err error, msg string) error {
+	return c.JSON(fiber.Map{
+		"code":    code,
+		"error":   err,
+		"message": msg,
+	})
+}
